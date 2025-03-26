714a5c8c0ff93ba18792a9bc2c842f2c6049e564
Rafly Aziz Abdillah <raflyazizabdillah30@gmail.com>
2025-03-27 00:13:55 +0700

    Refactor LoginPage component to replace LoginBanner with AuthHero for improved layout and user experience

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,17 +1,15 @@
 "use client";
 
-import { LoginBanner } from "@/components/LoginBanner";
 import { LoginForm } from "@/components/LoginForm";
 import { GlobalAlert } from "@/components/GlobalAlert";
+import { AuthHero } from "@/components/auth/AuthHero";
 
 export default function LoginPage() {
   return (
     <>
       <GlobalAlert />
       <div className="flex h-screen w-full">
-        <div className="hidden lg:block lg:w-1/2">
-          <LoginBanner />
-        </div>
+        <AuthHero />
         <div className="w-full lg:w-1/2 flex items-center justify-center">
           <LoginForm />
         </div>
